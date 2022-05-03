f0955b1ec388f19584956145c6449a4d20e9be88
AnderSantos7 <56338314+AnderSantos7@users.noreply.github.com>
2022-05-03 08:33:04 +0200

    SpringProject_DDBBCreationScript

---
+++ DDBB/MUgitu_DDBB_CreationScript.sql
@@ -0,0 +1,104 @@
+drop database if exists mugitu;
+create database mugitu;
+use mugitu;
+
+create table bici(
+    bici_id bigint,
+    modelo nvarchar(128),
+    estado boolean,
+    electrica boolean,
+    SOC int check ( SOC <= 100 && SOC >= 0 )
+);
+
+create table estacion(
+    estacion_id bigint,
+    nombre nvarchar(128),
+    coordenadas point,
+    plazas int,
+    activa boolean
+);
+
+create table usuario(
+    user_id bigint,
+    nombre nvarchar(128),
+    apellidos nvarchar(128),
+    correo nvarchar(128),
+    telefono nvarchar(16),
+    DNI nvarchar(16),
+    tarjeta_pago nvarchar(16),
+    numero_seguridad nvarchar(3),
+    fecha_caducidad date,
+    sexo int,
+    password nvarchar(128),
+    fecha_nacimiento date,
+    tipo_usuario int
+);
+
+create table notificacion_averia(
+    notificacion_id bigint,
+    bici_id bigint,
+    user_id bigint,
+    fecha datetime,
+    tipo_averia int
+);
+
+create table averia(
+    averia_id bigint,
+    fecha_inicio datetime,
+    fecha_fin datetime,
+    bici_id bigint,
+    tipo_averia int
+);
+
+create table estacionamiento(
+    estacionamiento_id bigint,
+    estacion_id bigint,
+    bici_id bigint(128),
+    fecha_inicio datetime,
+    fecha_fin datetime
+);
+
+create table evento(
+    evento_id bigint,
+    bici_id bigint,
+    estado int,
+    fecha datetime
+);
+
+create table utilizacion(
+    utiliza_id bigint,
+    bici_id bigint,
+    user_id bigint,
+    fecha_inicio datetime,
+    fecha_fin datetime
+);
+
+#Create primary key
+ALTER TABLE usuario ADD CONSTRAINT pk_user PRIMARY KEY (user_id);
+ALTER TABLE bici ADD CONSTRAINT pk_bici PRIMARY KEY (bici_id);
+ALTER TABLE estacion ADD CONSTRAINT pk_estacion PRIMARY KEY (estacion_id);
+ALTER TABLE estacionamiento ADD CONSTRAINT pk_estacionamiento PRIMARY KEY (estacionamiento_id);
+ALTER TABLE notificacion_averia ADD CONSTRAINT pk_notificacion PRIMARY KEY (notificacion_id);
+ALTER TABLE averia ADD CONSTRAINT pk_averia PRIMARY KEY (averia_id);
+ALTER TABLE evento ADD CONSTRAINT pk_evento PRIMARY KEY (evento_id);
+ALTER TABLE utilizacion ADD CONSTRAINT pk_utilizacion PRIMARY KEY (utiliza_id);
+
+#Set Auto Increments
+ALTER TABLE usuario MODIFY COLUMN user_id BIGINT auto_increment;
+ALTER TABLE bici MODIFY COLUMN bici_id BIGINT auto_increment;
+ALTER TABLE estacion MODIFY COLUMN estacion_id BIGINT auto_increment;
+ALTER TABLE estacionamiento MODIFY COLUMN estacionamiento_id BIGINT auto_increment;
+ALTER TABLE notificacion_averia MODIFY COLUMN notificacion_id BIGINT auto_increment;
+ALTER TABLE averia MODIFY averia_id BIGINT auto_increment;
+ALTER TABLE evento MODIFY evento_id BIGINT auto_increment;
+ALTER TABLE utilizacion MODIFY utiliza_id BIGINT auto_increment;
+
+#Create foreign key
+ALTER TABLE estacionamiento ADD CONSTRAINT fk_estacionamiento_estacion FOREIGN KEY (estacion_id) REFERENCES estacion(estacion_id);
+ALTER TABLE estacionamiento ADD CONSTRAINT fk_estacionamiento_bici FOREIGN KEY (bici_id) REFERENCES bici(bici_id);
+ALTER TABLE notificacion_averia ADD CONSTRAINT fk_notificacion_user FOREIGN KEY (user_id) REFERENCES usuario(user_id);
+ALTER TABLE notificacion_averia ADD CONSTRAINT fk_notificacion_bici FOREIGN KEY (bici_id) REFERENCES bici(bici_id);
+ALTER TABLE averia ADD CONSTRAINT fk_averia_bici FOREIGN KEY (bici_id) REFERENCES bici (bici_id);
+ALTER TABLE evento ADD CONSTRAINT fk_evento_bici FOREIGN KEY (bici_id) REFERENCES bici (bici_id);
+ALTER TABLE utilizacion ADD CONSTRAINT fk_utiliza_user FOREIGN KEY (user_id) REFERENCES usuario (user_id);
+ALTER TABLE utilizacion ADD CONSTRAINT fk_utiliza_bici FOREIGN KEY (bici_id) REFERENCES bici (bici_id);
---
+++ Web_app/MUgitu/src/main/java/Mooving/MUgitu/MUgituApplication.java
@@ -0,0 +1,13 @@
+package Mooving.MUgitu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MUgituApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MUgituApplication.class, args);
+	}
+
+}
